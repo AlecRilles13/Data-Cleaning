fa65b84fe6a0f03f8fc82a3a23bab4f425fe0a53
Alec Rilles <alecmrilles@gmail.com>
2024-07-17 02:15:44 +0800

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -4,13 +4,6 @@ SELECT *
 FROM layoffs;
 
 
--- 1. Remove Duplicates
--- 2. Standardize the Data
--- 3. Null Values or blank values
--- 4. Remove Any Columns
-
-
-
 CREATE TABLE layoffs_staging
 LIKE layoffs;
 
